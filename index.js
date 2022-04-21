ff3a04e5cf0b8c7644ff8c58e1426aece6e83226
paridaabinash <abinashparida833@gmail.com>
2022-04-21 23:27:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Create a button , on click of which you should be able to hide a paragraph text, create another button onclick of which this paragraph should be visible
+
+$("#btn1").click(function() {
+    $("p").hide();
+})
+
+$("#btn2").click(function() {
+    $("p").show();
+})
+
+// Create an ajax call, by using this api "https://jsonplaceholder.typicode.com/todos/12" and print the title as the heading of the page
+
+$("#btn3").click(function() {
+    $.ajax({
+        url: "https://jsonplaceholder.typicode.com/todos/12",
+        type: "GET",
+        success: function(data) {
+            
+            document.getElementById("h1").innerText = data.title;
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
